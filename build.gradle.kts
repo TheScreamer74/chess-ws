a6ff20b83d34dbafabedc2749c5087935a0b96ff
TheScreamer74 <blakilleur@gmail.com>
2024-10-11 22:27:13 +0200

    fix
    change postgre version due to vulnerability

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
     implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
 
-    implementation("org.postgresql:postgresql:42.5.0") // PostgreSQL driver
+    implementation("org.postgresql:postgresql:42.7.2") // PostgreSQL driver
     implementation("com.zaxxer:HikariCP:5.0.1") // Connection pool
 
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
